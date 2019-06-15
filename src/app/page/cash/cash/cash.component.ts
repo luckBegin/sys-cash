dfe0ddc6eb3c423e0c8f3db556e4d5f3ed26372d
Sirius.Black <ofghrt@qq.com>
2019-06-15 10:05:51 +0800

    UPDATE

--- src/app/page/cash/cash/cash.component.ts
+++ src/app/page/cash/cash/cash.component.ts
@@ -23,7 +23,8 @@ export class CashComponent implements OnInit, OnDestroy {
 		private readonly service: RoomService,
 		private readonly msg: MsgService,
 		private readonly keyboardSer: KeyboardService
-	) {}
+	) {
+	}
 	
 	ngOnInit(): void {
 		this.getENUMs();
@@ -62,9 +63,9 @@ export class CashComponent implements OnInit, OnDestroy {
 	
 	selectType: any = 'type';
 	selectItem: any = 'all';
-	selectTabIndex: number = 1 ; // 0 计时  1 买断 2 预买
-	private serveTimeStamp: number ;
-	roomTime : any = {
+	selectTabIndex: number = 1; // 0 计时  1 买断 2 预买
+	private serveTimeStamp: number;
+	roomTime: any = {
 		time: [], // 计时
 		outright: [], // 买断
 		advance: [], // 预买
@@ -154,8 +155,8 @@ export class CashComponent implements OnInit, OnDestroy {
 		this.dataList = arr;
 	}
 	
-	switchType($event: NzTabChangeEvent ): void {
-		const type = $event.index === 0 ? "type" : "area" ;
+	switchType($event: NzTabChangeEvent): void {
+		const type = $event.index === 0 ? "type" : "area";
 		this.selectType = type;
 		this.dataList = this.rawData[type];
 		this.selectItem = 'all';
@@ -172,20 +173,20 @@ export class CashComponent implements OnInit, OnDestroy {
 	selectRoomItem: any = null;
 	
 	@ViewChild("outrightComponent")
-	outrightComponent: OutrightComponent ;
+	outrightComponent: OutrightComponent;
 	
 	@ViewChild('advanceComponent')
-	advanceComponent: AdvanceComponent ;
+	advanceComponent: AdvanceComponent;
 	
 	@ViewChild('checkoutRoomComponent')
-	checkoutRoomComponent:CheckoutRoomComponent ;
+	checkoutRoomComponent: CheckoutRoomComponent;
 	
 	roomSelect(item: any): void {
 		if (this.selectRoomId === item.id) {
 			this.keyEventEnabled = false;
 			
-			if( item.status === 0 ) {
-				this.getPriceByType( item.typeId ) ;
+			if (item.status === 0) {
+				this.getPriceByType(item.typeId);
 			} else {
 				this.modalShow = true;
 			}
@@ -204,142 +205,144 @@ export class CashComponent implements OnInit, OnDestroy {
 			});
 	}
 	
-	getPriceByType( typeId: number ): void{
-		this.service.getPriceByType( { typeId })
-			.subscribe( ( res: RESPONSE ) => {
-				const time = [] ;
-				const outright = [] ;
-				const advance = [] ;
+	getPriceByType(typeId: number): void {
+		this.service.getPriceByType({typeId})
+			.subscribe((res: RESPONSE) => {
+				const time = [];
+				const outright = [];
+				const advance = [];
 				// 判断是否每个分类可用
-				res.data.forEach( item => {
-					if( item.isTime === '1')
-						time.push( item ) ;
+				res.data.forEach(item => {
+					if (item.isTime === '1')
+						time.push(item);
 					
-					if( item.isOutright === '1')
-						outright.push( item ) ;
+					if (item.isOutright === '1')
+						outright.push(item);
 					
-					if( item.isAdvance === '1')
-						advance.push( item ) ;
+					if (item.isAdvance === '1')
+						advance.push(item);
 				});
-				this.roomTime = { time , outright , advance } ;
+				this.roomTime = {time, outright, advance};
 				
-				if( this.outrightComponent )
-					this.outrightComponent.init() ;
+				if (this.outrightComponent)
+					this.outrightComponent.init();
 				
-				if( this.advanceComponent){
-					this.advanceComponent.init() ;
+				if (this.advanceComponent) {
+					this.advanceComponent.init();
 				}
 				
-				if( this.checkoutRoomComponent){
-					this.checkoutRoomComponent.init() ;
+				if (this.checkoutRoomComponent) {
+					this.checkoutRoomComponent.init();
 				}
 				
 				this.modalShow = true;
-			} , e => {
-				this.msg.error('查询房间价格异常' + e ) ;
+			}, e => {
+				this.msg.error('查询房间价格异常' + e);
 			})
 	}
 	
 	
-	@Strategy( {
-		0 : function( $event: MouseEvent ){ (this as CashComponent ).openByTime($event) }
+	@Strategy({
+		0: function ($event: MouseEvent) {
+			(this as CashComponent).openByTime($event)
+		}
 	})
-	open( $event: Event ): number {
-		return this.selectTabIndex ;
+	open($event: Event): number {
+		return this.selectTabIndex;
 	}
 	
-	@Service("service.openByTime" , true , function(){
-		return { id: (this as CashComponent).selectRoomItem.id } ;
+	@Service("service.openByTime", true, function () {
+		return {id: (this as CashComponent).selectRoomItem.id};
 	})
-	openByTime( $event : MouseEvent ): void{
+	openByTime($event: MouseEvent): void {
 		this.msg.success("开房成功");
-		this.modalShow = false ;
-		this.getList() ;
+		this.modalShow = false;
+		this.getList();
 	};
 	
-	checkoutShow: boolean = false ;
+	checkoutShow: boolean = false;
 	
-	checkout($event: any ): void{
-		if( this.selectTabIndex === 1 ) {
-			const withTimePrice = this.outrightComponent.getWithTimePrice() ;
-			const selectOutright = this.outrightComponent.getSelectOutright() ;
-			this.checkoutRoomComponent.makeCalc(this.selectTabIndex , withTimePrice , selectOutright) ;
-			this.checkoutRoomComponent.initPaymentMethod() ;
+	checkout($event: any): void {
+		if (this.selectTabIndex === 1) {
+			const withTimePrice = this.outrightComponent.getWithTimePrice();
+			const selectOutright = this.outrightComponent.getSelectOutright();
+			this.checkoutRoomComponent.makeCalc(this.selectTabIndex, withTimePrice, selectOutright);
+			this.checkoutRoomComponent.initPaymentMethod();
 		} else {
-			const time = this.advanceComponent.getTime() ;
-			this.checkoutRoomComponent.makeCalc(2, true , {
-				shopId: this.selectRoomItem.shopId ,
-				typeId: this.selectRoomItem.typeId ,
-				startTime: time.joinTime ,
+			const time = this.advanceComponent.getTime();
+			this.checkoutRoomComponent.makeCalc(2, true, {
+				shopId: this.selectRoomItem.shopId,
+				typeId: this.selectRoomItem.typeId,
+				startTime: time.joinTime,
 				endTime: time.leaveTime
-			}) ;
-			this.checkoutRoomComponent.initPaymentMethod() ;
+			});
+			this.checkoutRoomComponent.initPaymentMethod();
 		}
-		this.checkoutShow = true ;
+		this.checkoutShow = true;
 	}
 	
-	checkoutByTime( $event: any ): void{
-		this.checkoutRoomComponent.makeCalc(0, true , this.selectRoomItem ) ;
-		this.checkoutShow = true ;
-		this.checkoutRoomComponent.initPaymentMethod() ;
+	checkoutByTime($event: any): void {
+		this.checkoutRoomComponent.makeCalc(0, true, this.selectRoomItem);
+		this.checkoutShow = true;
+		this.checkoutRoomComponent.initPaymentMethod();
 	}
 	
-	@Service('service.openByOutright' , true , function(){
-		const methodPayment = ( this as CashComponent ).checkoutRoomComponent.getMethodPayment() ;
-		methodPayment['id'] = ( this as CashComponent ).selectRoomId ;
-		return methodPayment ;
+	@Service('service.openByOutright', true, function () {
+		const methodPayment = (this as CashComponent).checkoutRoomComponent.getMethodPayment();
+		methodPayment['id'] = (this as CashComponent).selectRoomId;
+		return methodPayment;
 	})
-	openByOutright($event: MouseEvent): void{
-		this.checkoutShow = false ;
-		this.modalShow = false ;
-		this.msg.success('操作成功') ;
-		this.getList() ;
+	openByOutright($event: MouseEvent): void {
+		this.checkoutShow = false;
+		this.modalShow = false;
+		this.msg.success('操作成功');
+		this.getList();
 	}
 	
-	@Service('service.openByAdvance' , true , function(){
-		const methodPayment = ( this as CashComponent ).checkoutRoomComponent.getMethodPayment() ;
-		methodPayment['id'] = ( this as CashComponent ).selectRoomId ;
-		return methodPayment ;
+	@Service('service.openByAdvance', true, function () {
+		const methodPayment = (this as CashComponent).checkoutRoomComponent.getMethodPayment();
+		methodPayment['id'] = (this as CashComponent).selectRoomId;
+		return methodPayment;
 	})
-	openByAdvance($event: MouseEvent): void{
-		this.checkoutShow = false ;
-		this.modalShow = false ;
-		this.msg.success('操作成功') ;
-		this.getList() ;
+	openByAdvance($event: MouseEvent): void {
+		this.checkoutShow = false;
+		this.modalShow = false;
+		this.msg.success('操作成功');
+		this.getList();
 	}
 	
-	@Service('service.checkoutWithTime' , true , function(){
-		const methodPayment = (this as CashComponent).checkoutRoomComponent.getMethodPayment() ;
-		methodPayment['id'] = (this as CashComponent).selectRoomId ;
-		methodPayment['orderId'] = ( this as CashComponent ).selectRoomItem.orderId ;
-		return methodPayment ;
+	@Service('service.checkoutWithTime', true, function () {
+		const methodPayment = (this as CashComponent).checkoutRoomComponent.getMethodPayment();
+		methodPayment['id'] = (this as CashComponent).selectRoomId;
+		methodPayment['orderId'] = (this as CashComponent).selectRoomItem.orderId;
+		return methodPayment;
 	})
-	checkOutWithTime($event: MouseEvent): void{
-		this.checkoutShow = false ;
-		this.modalShow = false ;
-		this.msg.success('操作成功') ;
-		this.getList() ;
+	checkOutWithTime($event: MouseEvent): void {
+		this.checkoutShow = false;
+		this.modalShow = false;
+		this.msg.success('操作成功');
+		this.getList();
 	}
 	
-	@Service('service.reset' , true , function(){
+	@Service('service.reset', true, function () {
 		return {
 			id: (this as CashComponent).selectRoomItem.id
 		}
 	})
-	reset( $event: MouseEvent): void{
-		this.msg.success("操作成功") ;
+	reset($event: MouseEvent): void {
+		this.msg.success("操作成功");
 		this.getList();
-		this.modalShow = false ;
+		this.modalShow = false;
 	}
 	
-	@Service('service.clean' , true , function(){
+	@Service('service.clean', true, function () {
 		return {
 			id: (this as CashComponent).selectRoomItem.id
 		}
 	})
-	clean( $event: MouseEvent): void{
-		this.msg.success("操作成功") ;
+	clean($event: MouseEvent): void {
+		this.msg.success("操作成功");
 		this.getList();
-		this.modalShow = false ;
+		this.modalShow = false;
 	}
 }
