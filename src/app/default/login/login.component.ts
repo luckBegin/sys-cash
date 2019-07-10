fe8c8bf657bb89d6cfcbf3817417051aaaf7a9dd
Sirius.Black <ofghrt@qq.com>
2019-07-10 16:41:45 +0800

    [修改]登录页样式

--- src/app/default/login/login.component.ts
+++ src/app/default/login/login.component.ts
@@ -1,87 +1,90 @@
-
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {MsgService, SesssionStorageService, StaffService} from '../../service';
 import {RESPONSE} from '../../models' ;
 import {Service} from '../../../decorators' ;
 import {Router} from '@angular/router' ;
-import {WeChatService} from "../../service/weChat/weChat.service";
-import {interval, Observable} from "rxjs";
+import {WeChatService} from '../../service/weChat/weChat.service';
+import {interval, Observable} from 'rxjs';
+import {CONFIG} from '../../CONFIG';
 
 @Component({
 	selector: 'login',
 	templateUrl: './login.component.html',
 	styleUrls: ['./login.component.less']
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit {
 	constructor(
 		private readonly fb: FormBuilder,
 		private readonly msg: MsgService,
-		private readonly service: StaffService ,
-		private readonly sgo: SesssionStorageService ,
-		private readonly router: Router ,
+		private readonly service: StaffService,
+		private readonly sgo: SesssionStorageService,
+		private readonly router: Router,
 		private readonly wxSer: WeChatService
-	) {}
-
+	) {
+	}
+	
 	form: FormGroup = this.fb.group({
 		username: [null, [Validators.required]],
 		password: [null, [Validators.required]]
 	});
 	
-	qrStr: string = '' ;
-	private loginKey: string = '' ;
-	private wxLoginEvent$ ;
+	qrStr: string = '';
+	private loginKey: string = '';
+	private wxLoginEvent$;
+	private CONFIG = CONFIG ;
 	ngOnInit(): void {
-		this.getQrCode() ;
+		this.getQrCode();
 	}
+	
 	@Service('service.login', true, function() {
-		const THIS = this as LoginComponent ;
-		if ( !THIS.form.valid ) {
+		const THIS = this as LoginComponent;
+		if (!THIS.form.valid) {
 			THIS.msg.warn('请输入账号和密码');
 			return false;
 		} else {
 			return THIS.form.value;
 		}
 	})
-	login($event: MouseEvent , res?: RESPONSE ): void {
-		this.wxLoginEvent$.unsubscribe() ;
-		const menu = res.data.menuInfo[1].children as any[] ;
-		if ( menu.length <= 0 ) {
-			this.msg.warn('该账号不具备任何权限,请联系管理人员') ;
-			return ;
+	login($event: MouseEvent, res?: RESPONSE): void {
+		this.wxLoginEvent$.unsubscribe();
+		const menu = res.data.menuInfo[1].children as any[];
+		if (menu.length <= 0) {
+			this.msg.warn('该账号不具备任何权限,请联系管理人员');
+			return;
 		}
-		this.sgo.set('loginInfo' , res.data) ;
-
-		this.router.navigate(['/prelogin']) ;
+		this.sgo.set('loginInfo', res.data);
+		
+		this.router.navigate(['/prelogin']);
 	}
 	
-	getQrCode(): void{
+	getQrCode(): void {
 		this.wxSer.getQrCode()
-			.subscribe( ( res: RESPONSE ) => {
-				this.qrStr = res.data.qrUrl ;
-				this.loginKey = res.data.key ;
-				if( !this.wxLoginEvent$ ){
-					this.wxLoginEvent$ = interval(2000)
-						.subscribe( res => {
-							this.onWxLogin() ;
-						})
-				}
-			});
+		.subscribe((res: RESPONSE) => {
+			this.qrStr = res.data.qrUrl;
+			this.loginKey = res.data.key;
+			if (!this.wxLoginEvent$) {
+				this.wxLoginEvent$ = interval(2000)
+				.subscribe(res => {
+					this.onWxLogin();
+				});
+			}
+		});
 	}
 	
-	onWxLogin(): void{
-		this.service.qrLogin({ code : this.loginKey})
-			.subscribe( ( res : RESPONSE ) => {
-				if( res.success === true ) {
-					this.wxLoginEvent$.unsubscribe() ;
-					const menu = res.data.menuInfo[1].children as any[] ;
-					if ( menu.length <= 0 ) {
-						this.msg.warn('该账号不具备任何权限,请联系管理人员') ;
-						return ;
-					}
-					this.sgo.set('loginInfo' , res.data) ;
-					this.router.navigate(['/prelogin']) ;
+	onWxLogin(): void {
+		this.service.qrLogin({code: this.loginKey})
+		.subscribe((res: RESPONSE) => {
+			if (res.success === true) {
+				this.wxLoginEvent$.unsubscribe();
+				const menu = res.data.menuInfo[1].children as any[];
+				if (menu.length <= 0) {
+					this.msg.warn('该账号不具备任何权限,请联系管理人员');
+					return;
 				}
-			});
+				this.sgo.set('loginInfo', res.data);
+				this.router.navigate(['/prelogin']);
+			}
+		});
 	}
 }
