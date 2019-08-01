c8fb27065c87f5ae27c8851c18122a51d0c67412
Sirius.Black <ofghrt@qq.com>
2019-08-01 14:28:43 +0800

    [增加]会员积分查询模块

--- src/app/service/websocket/websocket.service.ts
+++ src/app/service/websocket/websocket.service.ts
@@ -1,6 +1,6 @@
 import {Injectable, OnInit} from '@angular/core';
 import {API} from '../API';
-import {interval, Subject} from 'rxjs';
+import {interval, Subject, Subscription} from 'rxjs';
 
 export interface WsEvent {
 	event: string;
@@ -11,55 +11,11 @@ export interface WsEvent {
 export class WebsocketService {
 	private ws!: WebSocket;
 	private state: { state: boolean , msg: string } ;
+	private reConnect!: Subscription;
 	public readonly WSEvent$: Subject<WsEvent> = new Subject();
 	constructor(
 	) {
-		try {
-			this.ws = new WebSocket(API.WSUrl);
-		} catch (e) {
-			this.state = {
-				state: false ,
-				msg: '客户端未启动'
-			};
-			this.notify('state' , this.state ) ;
-		}
-		
-		this.ws.onopen = () => {
-			this.state = {
-				state: true ,
-				msg: '连接成功'
-			};
-			this.notify('state' , this.state ) ;
-			this.heartBeat();
-		};
-		
-		this.ws.onerror = (e) => {
-			this.state =  {
-				state: false ,
-				msg: '连接异常'
-			};
-			this.notify('state' , this.state ) ;
-		};
-		
-		this.ws.onclose = ( e ) => {
-			this.state = {
-				state: false ,
-				msg: '客户端断开连接'
-			} ;
-			this.notify('state' , this.state ) ;
-		};
-		
-		this.ws.onmessage = ( event: MessageEvent ) => {
-			const msg = event.data ;
-			try {
-				const objMsg = JSON.parse( msg ) ;
-				if ( objMsg.event !== 'heartBeat') {
-					this.notify( objMsg.event , objMsg.data ) ;
-				}
-			} catch (e) {
-				console.log( e ) ;
-			}
-		};
+		this.createWs() ;
 	}
 	
 	public getState(): { state: boolean , msg: string } {
@@ -79,5 +35,64 @@ export class WebsocketService {
 			.subscribe( () => {
 				this.sendMsg('heartBeat' , '') ;
 			});
+	}
+	
+	private createWs(): void {
+		try {
+			this.ws = new WebSocket(API.WSUrl);
+		} catch (e) {
+			this.state = {
+				state: false ,
+				msg: '客户端未启动'
+			};
+			this.notify('state' , this.state ) ;
+		}
+		
+		this.ws.onopen = () => {
+			this.state = {
+				state: true ,
+				msg: '连接成功'
+			};
+			
+			if ( this.reConnect) {
+				this.reConnect.unsubscribe() ;
+			}
+			
+			this.notify('state' , this.state ) ;
+			this.heartBeat();
+		};
+		
+		this.ws.onerror = (e) => {
+			this.state =  {
+				state: false ,
+				msg: '连接异常'
+			};
+			this.notify('state' , this.state ) ;
+		};
+		
+		this.ws.onclose = ( e ) => {
+			this.state = {
+				state: false ,
+				msg: '客户端断开连接'
+			} ;
+			this.notify('state' , this.state ) ;
+			
+			this.reConnect = interval( 300000 )
+			.subscribe( () => {
+				this.createWs() ;
+			});
+		};
+		
+		this.ws.onmessage = ( event: MessageEvent ) => {
+			const msg = event.data ;
+			try {
+				const objMsg = JSON.parse( msg ) ;
+				if ( objMsg.event !== 'heartBeat') {
+					this.notify( objMsg.event , objMsg.data ) ;
+				}
+			} catch (e) {
+				console.log( e ) ;
+			}
+		};
 	}
 }
